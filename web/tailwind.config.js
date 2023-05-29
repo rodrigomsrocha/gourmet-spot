a2c1cf6c9ffc3fbbd6e56af4f21c2c8fb192e48b
rodrigomsrocha <rodrigomtc82@gmail.com>
2023-05-29 01:58:57 -0300

    set up font and eslint

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -7,10 +7,37 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: 'var(--font-nunito-sans)',
+        alt: 'var(--font-righteous)',
+      },
+      colors: {
+        pink: {
+          50: '#fff0f1',
+          100: '#ffe2e4',
+          200: '#ffc9d1',
+          300: '#ff9caa',
+          400: '#ff657e',
+          500: '#ff2f56',
+          600: '#f4194c',
+          700: '#cd0337',
+          800: '#ac0535',
+          900: '#920935',
+          950: '#520017',
+        },
+        gray: {
+          50: '#f8f8f8',
+          100: '#f1f0ef',
+          200: '#e6e3e2',
+          300: '#d4d0cd',
+          400: '#bab3af',
+          500: '#a09893',
+          600: '#887f7a',
+          700: '#716964',
+          800: '#5f5955',
+          900: '#524d4a',
+          950: '#2a2725',
+        },
       },
     },
   },
