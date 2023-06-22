af8635ca122db2e1f5e535420a4c366140816bfb
rodrigomsrocha <rodrigomtc82@gmail.com>
2023-06-22 00:21:25 -0300

    show numberof orders badge

--- web/src/components/Table.tsx
+++ web/src/components/Table.tsx
@@ -37,8 +37,11 @@ export async function Table({ table }: TableProps) {
       <div className="grid grid-cols-table grid-rows-table place-items-center gap-1 cursor-pointer">
         <div className="rounded-lg col-start-2 w-20 h-4 bg-pink-400"></div>
         <div className="rounded-lg w-full col-start-1 row-start-2 h-20 bg-pink-400"></div>
-        <div className="rounded-[2.5rem] w-full col-start-2 h-40 row-start-2 flex items-center justify-center bg-pink-400">
+        <div className="rounded-[2.5rem] w-full col-start-2 h-40 row-start-2 flex items-center justify-center bg-pink-400 relative">
           <strong className="text-2xl text-gray-950">M{table.number}</strong>
+          <span className="w-5 h-5 rounded-full bg-pink-400 top-0 right-0 absolute font-bold flex items-center justify-center text-sm border-2 border-gray-950">
+            {orders.length}
+          </span>
         </div>
         <div className="rounded-lg w-full col-start-3 row-start-2 h-20 bg-pink-400"></div>
         <div className="rounded-lg col-start-2 row-start-3 w-20 h-4 bg-pink-400"></div>
