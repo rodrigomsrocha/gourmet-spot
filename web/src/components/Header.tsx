a8a7c48850527967176ff7dd211fb011742cfd82
rodrigomsrocha <rodrigomtc82@gmail.com>
2023-07-01 23:42:59 -0300

    create client modal component

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import logo from '../assets/logo.svg'
 import { UserPlus } from 'lucide-react'
 import Link from 'next/link'
+import { CreateClientModal } from './CreateClientModal'
 
 export function Header() {
   return (
@@ -16,9 +17,11 @@ export function Header() {
         </Link>
       </nav>
       <div className="flex gap-5">
-        <button className="border-2 border-pink-400 bg-pink-100 shadow-md px-3 rounded-lg flex items-center justify-center transition-colors hover:bg-pink-200">
-          <UserPlus className="h-6 w-6" />
-        </button>
+        <CreateClientModal>
+          <button className="h-full border-2 border-pink-400 bg-pink-100 shadow-md px-3 rounded-lg flex items-center justify-center transition-colors hover:bg-pink-200">
+            <UserPlus className="h-6 w-6" />
+          </button>
+        </CreateClientModal>
         <button className="bg-pink-400 rounded-lg text-lg font-bold py-3 px-10 shadow-md transition-colors hover:bg-pink-500">
           criar reserva
         </button>
